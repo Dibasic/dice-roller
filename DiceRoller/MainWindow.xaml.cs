6b9c1cf5bf6114421daa81f2a52ceeed054bf75e
Dibasic <potatobackpack@gmail.com>
2018-01-01 12:37:51 -0600

    Initial commit

---
+++ DiceRoller/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DiceRollerLogicLayer;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRoll_Click(object sender, RoutedEventArgs e)
+        {
+            var dm = new DiceManager()
+            {
+                d2 = (int)d2.Value,
+                d3 = (int)d3.Value,
+                d4 = (int)d4.Value,
+                d6 = (int)d6.Value,
+                d8 = (int)d8.Value,
+                d10 = (int)d10.Value,
+                d12 = (int)d12.Value,
+                d20 = (int)d20.Value,
+                d100 = (int)d100.Value,
+                dx = (int)dx.Value,
+                x = (int)x.Value,
+                add = (int)add.Value
+            };
+            btnRoll.Content = dm.Roll();
+            dm = null;
+        }
+    }
+}
---
+++ DiceRollerLogicLayer/DiceManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerLogicLayer
+{
+    public class DiceManager
+    {
+        public int d2 { get; set; }
+        public int d3 { get; set; }
+        public int d4 { get; set; }
+        public int d6 { get; set; }
+        public int d8 { get; set; }
+        public int d10 { get; set; }
+        public int d12 { get; set; }
+        public int d20 { get; set; }
+        public int d100 { get; set; }
+        public int dx { get; set; }
+        public int x { get; set; }
+        public int add { get; set; }
+        public DiceManager()
+        {
+            
+        }
+        public int Roll()
+        {
+            int result = 0;
+
+            Random rng = new Random();
+
+            for (int i = 0; i < d2; i++)
+            {
+                result += rng.Next(1, 3);
+            }
+
+            for (int i = 0; i < d3; i++)
+            {
+                result += rng.Next(1, 4);
+            }
+
+            for (int i = 0; i < d4; i++)
+            {
+                result += rng.Next(1, 5);
+            }
+
+            for (int i = 0; i < d6; i++)
+            {
+                result += rng.Next(1, 7);
+            }
+
+            for (int i = 0; i < d8; i++)
+            {
+                result += rng.Next(1, 9);
+            }
+
+            for (int i = 0; i < d10; i++)
+            {
+                result += rng.Next(1, 11);
+            }
+
+            for (int i = 0; i < d12; i++)
+            {
+                result += rng.Next(1, 13);
+            }
+
+            for (int i = 0; i < d20; i++)
+            {
+                result += rng.Next(1, 21);
+            }
+
+            for (int i = 0; i < d100; i++)
+            {
+                result += rng.Next(1, 101);
+            }
+
+            if (x > 0)
+            {
+                for (int i = 0; i < dx; i++)
+                {
+                    result += rng.Next(1, x+1);
+                }
+            }
+
+            result += add;
+
+            return result;
+        }
+    }
+}
