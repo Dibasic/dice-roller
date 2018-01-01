1ca3855d1043d3a70077570ec2d87471ae63b746
Dibasic <potatobackpack@gmail.com>
2018-01-01 13:16:22 -0600

    Simplifies DiceManager logic

--- DiceRollerLogicLayer/DiceManager.cs
+++ DiceRollerLogicLayer/DiceManager.cs
@@ -20,71 +20,49 @@ namespace DiceRollerLogicLayer
         public int dx { get; set; }
         public int x { get; set; }
         public int add { get; set; }
+
+        private Random rng;
         public DiceManager()
         {
-            
+            rng = new Random();
         }
         public int Roll()
         {
             int result = 0;
 
-            Random rng = new Random();
-
-            for (int i = 0; i < d2; i++)
-            {
-                result += rng.Next(1, 3);
-            }
-
-            for (int i = 0; i < d3; i++)
-            {
-                result += rng.Next(1, 4);
-            }
-
-            for (int i = 0; i < d4; i++)
-            {
-                result += rng.Next(1, 5);
-            }
-
-            for (int i = 0; i < d6; i++)
-            {
-                result += rng.Next(1, 7);
-            }
-
-            for (int i = 0; i < d8; i++)
-            {
-                result += rng.Next(1, 9);
-            }
-
-            for (int i = 0; i < d10; i++)
-            {
-                result += rng.Next(1, 11);
-            }
-
-            for (int i = 0; i < d12; i++)
-            {
-                result += rng.Next(1, 13);
-            }
-
-            for (int i = 0; i < d20; i++)
-            {
-                result += rng.Next(1, 21);
-            }
-
-            for (int i = 0; i < d100; i++)
-            {
-                result += rng.Next(1, 101);
-            }
+            result += RollDice(2, d2);
+            result += RollDice(3, d3);
+            result += RollDice(4, d4);
+            result += RollDice(6, d6);
+            result += RollDice(8, d8);
+            result += RollDice(10, d10);
+            result += RollDice(12, d12);
+            result += RollDice(20, d20);
+            result += RollDice(100, d100);
 
             if (x > 0)
             {
-                for (int i = 0; i < dx; i++)
-                {
-                    result += rng.Next(1, x+1);
-                }
+                result += RollDice(x, dx);
             }
 
             result += add;
 
+            return result;
+        }
+        private int RollDice(int sides, int times)
+        {
+            int result = 0;
+
+            for (int i = 0; i < times; i++)
+            {
+                result += RollDie(sides);
+            }
+
+            return result;
+        }
+        private int RollDie(int sides)
+        {
+            int result = rng.Next(1, sides + 1);
             return result;
         }
     }
